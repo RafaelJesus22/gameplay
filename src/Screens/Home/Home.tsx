36f7798f6182314aa99a7d6309ed2347016fea0d
Rafael Jesus <rafael.moura22@fatec.sp.gov.br>
2021-06-23 23:30:34 -0300

    Home finalizada

--- src/Screens/Home/Home.tsx
+++ src/Screens/Home/Home.tsx
@@ -5,7 +5,8 @@ import { Profile } from '../../components/Profile/Profile';
 import { ButtonAdd } from '../../components/ButtonAdd/ButtonAdd';
 import { CategorySelect } from '../../components/CategorySelect/CategorySelect';
 import { ListHeader } from '../../components/ListHeader/ListHeader';
-import { Appointment } from '../../components/Appointment/Appointment'
+import { Appointment } from '../../components/Appointment/Appointment';
+import { ListDivider } from '../../components/ListDivider';
 import { styles } from './styles';
 
 
@@ -24,7 +25,19 @@ export function Home() {
       category: '1',
       date: '22/06 as 22h',
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam vero ab aliquid delectus. Reprehenderit, ullam?id: '
-    }
+    },
+    {
+      id: '2',
+      guild: {
+        id: '1',
+        name: 'lendarios',
+        icon: null,
+        owner: true
+      },
+      category: '1',
+      date: '22/06 as 22h',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam vero ab aliquid delectus. Reprehenderit, ullam?id: '
+    },
   ]
 
   const handleCategorySelect = (categoryId: string) => {
@@ -54,6 +67,9 @@ export function Home() {
           <FlatList 
             data={appointments}
             keyExtractor={item => item.id}
+            style={styles.matches}
+            showsVerticalScrollIndicator={false}
+            ItemSeparatorComponent={() => <ListDivider />}
             renderItem={({ item }) => (
               <Appointment 
                 data={item}
