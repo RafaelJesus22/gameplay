b50a4f2ef85c7e9b3151414ca75d30fc3cd492ed
Rafael Jesus <rafael.moura22@fatec.sp.gov.br>
2021-06-24 08:59:03 -0300

    Melhorando o card de categoria

--- src/components/Category/Category.tsx
+++ src/components/Category/Category.tsx
@@ -12,23 +12,45 @@ interface Props extends RectButtonProps{
   title: string;
   icon: React.FC<SvgProps>;
   checked?: boolean;
+  hasCheckBox?: boolean;
 }
 
 export const Category = (props: Props) => {
-  const { title, checked, icon: Icon, ...rest } = props;
-  const { secondary50, secondary70 } = theme.colors
+  const { 
+    title, 
+    checked, 
+    icon: Icon, 
+    hasCheckBox, 
+    ...rest 
+  } = props;
+  
+  const { 
+    secondary40, 
+    secondary50, 
+    secondary70, 
+    secondary85 
+  } = theme.colors
 
   return (
     <RectButton {...rest}>
       <LinearGradient
+        style={styles.container}
         colors={[
           secondary50,
           secondary70,
         ]}
-        style={styles.container}
       >
-        <View style={[styles.content, {opacity: checked ? 1 : 0.4}]}>
-          <View style={checked ? styles.checked : styles.check} />
+        <LinearGradient 
+          style={[styles.content, {opacity: checked ? 1 : 0.4}]}
+          colors={[
+            checked ? secondary85 : secondary50, 
+            secondary40
+          ]}
+        >
+          {
+            !hasCheckBox && 
+            <View style={checked ? styles.checked : styles.check} />
+          }
 
           <Icon 
             width={48}
@@ -38,7 +60,7 @@ export const Category = (props: Props) => {
           <Text style={styles.title}>
             {title}
           </Text>
-        </View>
+        </LinearGradient>
       </LinearGradient>
     </RectButton>
   );
